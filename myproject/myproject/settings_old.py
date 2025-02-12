cb19741e5df1101890732d5b62f2b6f270f3c0c2
Nelly Porvatova <dolgikhhel@yandex.ru>
2025-02-12 14:57:19 +0400

    3 commit

--- myproject/myproject/settings_old.py
+++ myproject/myproject/settings_old.py
@@ -10,11 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,14 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
-
+SECRET_KEY = 'django-insecure-gv*t0b-bwk3+=il0wgw+j5ady0@qpsdd2-4@^_$$fi2^4^*v8i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
@@ -83,15 +78,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'cloud7'),
-        'USER': os.getenv('DB_USER', 'post'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'helen'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': 'cloud7',
+        'USER': 'post',
+        'PASSWORD': 'helen',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -163,5 +157,4 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
 }
-
 
